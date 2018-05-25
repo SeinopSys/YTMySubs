d215475dedf2139d3ce6dbc5e1709c7f26813d62
SeinopSys <seinopsys@gmail.com>
2018-05-25 06:49:10 +0200

    Minor optimizations

--- package/background.js
+++ package/background.js
@@ -1,15 +1,13 @@
-var test = /^(https?:\/\/www\.youtube\.com)\/?(\?.+?)?(#.+?)?$/;
+var test = /^(https:\/\/www\.youtube\.com)\/?(\?.+?)?(#.+?)?$/;
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details){
 		if (details.method !== 'GET')
 			return;
 
 		var match =  details.url.match(test);
-		if (match !== null){
-			console.log('Caught', details.url);
+		if (match !== null)
 			return {redirectUrl: match[1]+'/feed/subscriptions'+(match[2]||'')+(match[3]||'')};
-		}
 	},
-	{urls: ['https://*.youtube.com/*']},
+	{urls: ['https://www.youtube.com/*']},
 	["blocking"]
 );
