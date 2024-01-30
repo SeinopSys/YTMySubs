5d272b51ddde78f854dc30fbe366d5d4ce1fae4d
SeinopSys <seinopsys@protonmail.com>
2024-01-30 20:35:46 +0100

    Update preparation script

--- prep.sh
+++ prep.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rm package.zip && cd ${DIR}/package && zip -r ../package.zip *
+if [ -f package.zip ]; then
+  rm package.zip
+  echo "Deleted existing archive"
+fi
+cd ${DIR}/package && zip -r ../package.zip *
